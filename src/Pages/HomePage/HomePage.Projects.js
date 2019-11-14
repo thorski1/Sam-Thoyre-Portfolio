95276a85a606377c7603e4b8c2cdda64f4365c9e
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-11-13 21:29:09 -0500

    add news site to homepage, took out color palettes

--- src/Pages/HomePage/HomePage.Projects.js
+++ src/Pages/HomePage/HomePage.Projects.js
@@ -1,38 +1,36 @@
 export const projects = [
-	{
-		name: "Code Party",
-		color: "blue",
-		description: `This is an app similar to Meetups, but specifically for people looking to create projects together in a home environment.  Users can host a "Code Party" where users will get together and create projects using a specific technology, such as React or Vue.`,
-		githubURL: "https://github.com/thorski1/CodePartyReact",
-		siteURL: "https://codeparty-c6656.firebaseapp.com",
-		image: "/assets/CodeParty.png"
-	},
-	{
-		name: "Progress Blog",
-		color: "purple",
-		description: `This a blog that I post an update everyday about what and how I'm learning. I created this blog using Vue, Nuxt, and Storyblok CMS.`,
-		githubURL: "https://github.com/thorski1/CodePartyReact",
-		siteURL: "https://thorski1.github.io/ProgressBlog/",
-		image: "/assets/ProgressBlog.png"
-	},
-	{
-		name: "E-Commerce Clothing Store",
-		color: "grey",
-		description:
-			"This is an incredibly comprehensive E-Commerce demo app that uses React, Redux, Firebase, and Stripe.",
-		githubURL:
-			"https://github.com/thorski1/ECommerceClothingReact",
-		siteURL: "https://sams-crwn-clothing.herokuapp.com/",
-		image: "/assets/CRWN.png"
-	},
-	{
-		name: "Color Palette",
-		color: "violet",
-		description:
-			"This project utilizes React, Material UI, and many other packages and serves as an aid in creating a color palette for developers",
-		githubURL:
-			"https://github.com/thorski1/ColorPaletteReact",
-		siteURL: "https://objective-borg-49ef3d.netlify.com",
-		image: "/assets/ColorPalette.png"
-	}
-];
+         {
+           name: "The News",
+           color: "blue",
+           description:
+             "I built this site without any class components and several custom hooks.  I used Material UI, React-Router, and React Pose in the making of this app. It calls the News API to populate the news",
+           githubURL: "https://github.com/thorski1/TheNewsReact",
+           siteURL: "https://the-simple-news.netlify.com",
+           image: "/assets/TheNews.png"
+         },
+         {
+           name: "Code Party",
+           color: "blue",
+           description: `This is an app similar to Meetups, but specifically for people looking to create projects together in a home environment.  Users can host a "Code Party" where users will get together and create projects using a specific technology, such as React or Vue.`,
+           githubURL: "https://github.com/thorski1/CodePartyReact",
+           siteURL: "https://codeparty-c6656.firebaseapp.com",
+           image: "/assets/CodeParty.png"
+         },
+         {
+           name: "Progress Blog",
+           color: "purple",
+           description: `This a blog that I post an update everyday about what and how I'm learning. I created this blog using Vue, Nuxt, and Storyblok CMS.`,
+           githubURL: "https://github.com/thorski1/CodePartyReact",
+           siteURL: "https://thorski1.github.io/ProgressBlog/",
+           image: "/assets/ProgressBlog.png"
+         },
+         {
+           name: "E-Commerce Clothing Store",
+           color: "grey",
+           description:
+             "This is an incredibly comprehensive E-Commerce demo app that uses React, Redux, Firebase, and Stripe.",
+           githubURL: "https://github.com/thorski1/ECommerceClothingReact",
+           siteURL: "https://sams-crwn-clothing.herokuapp.com/",
+           image: "/assets/CRWN.png"
+         }
+       ];
