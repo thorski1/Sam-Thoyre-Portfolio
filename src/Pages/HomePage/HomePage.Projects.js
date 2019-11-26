782c402aa7f82ae8eb043a6bd473e96c5547a12a
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-11-25 19:01:37 -0500

    updated homepage to have new blog

--- src/Pages/HomePage/HomePage.Projects.js
+++ src/Pages/HomePage/HomePage.Projects.js
@@ -1,36 +1,43 @@
 export const projects = [
-         {
-           name: "The News",
-           color: "blue",
-           description:
-             "I built this site without any class components and several custom hooks.  I used Material UI, React-Router, and React Pose in the making of this app. It calls the News API to populate the news",
-           githubURL: "https://github.com/thorski1/TheNewsReact",
-           siteURL: "https://the-simple-news.netlify.com",
-           image: "/assets/TheNews.png"
-         },
-         {
-           name: "Code Party",
-           color: "blue",
-           description: `This is an app similar to Meetups, but specifically for people looking to create projects together in a home environment.  Users can host a "Code Party" where users will get together and create projects using a specific technology, such as React or Vue.`,
-           githubURL: "https://github.com/thorski1/CodePartyReact",
-           siteURL: "https://codeparty-c6656.firebaseapp.com",
-           image: "/assets/CodeParty.png"
-         },
-         {
-           name: "Progress Blog",
-           color: "purple",
-           description: `This a blog that I post an update everyday about what and how I'm learning. I created this blog using Vue, Nuxt, and Storyblok CMS.`,
-           githubURL: "https://github.com/thorski1/CodePartyReact",
-           siteURL: "https://thorski1.github.io/ProgressBlog/",
-           image: "/assets/ProgressBlog.png"
-         },
-         {
-           name: "E-Commerce Clothing Store",
-           color: "grey",
-           description:
-             "This is an incredibly comprehensive E-Commerce demo app that uses React, Redux, Firebase, and Stripe.",
-           githubURL: "https://github.com/thorski1/ECommerceClothingReact",
-           siteURL: "https://sams-crwn-clothing.herokuapp.com/",
-           image: "/assets/CRWN.png"
-         }
-       ];
+					{
+						name: "Progress Blog",
+						color: "blue",
+						description: `This a blog that I post an update everyday about what and how I'm learning. I created this blog using Gatsby and Contentful`,
+						githubURL:
+							"https://github.com/thorski1/New-Gatsby-Progress-Blog",
+						siteURL:
+							"https://sam-thoyre-progress-blog.netlify.com/",
+						image: "/assets/ProgressBlog2.png"
+					},
+					{
+						name: "The News",
+						color: "blue",
+						description:
+							"I built this site without any class components and several custom hooks.  I used Material UI, React-Router, and React Pose in the making of this app. It calls the News API to populate the news",
+						githubURL:
+							"https://github.com/thorski1/TheNewsReact",
+						siteURL: "https://the-simple-news.netlify.com",
+						image: "/assets/TheNews.png"
+					},
+					{
+						name: "Code Party",
+						color: "blue",
+						description: `This is an app similar to Meetups, but specifically for people looking to create projects together in a home environment.  Users can host a "Code Party" where users will get together and create projects using a specific technology, such as React or Vue.`,
+						githubURL:
+							"https://github.com/thorski1/CodePartyReact",
+						siteURL:
+							"https://codeparty-c6656.firebaseapp.com",
+						image: "/assets/CodeParty.png"
+					},
+					{
+						name: "E-Commerce Clothing Store",
+						color: "grey",
+						description:
+							"This is an incredibly comprehensive E-Commerce demo app that uses React, Redux, Firebase, and Stripe.",
+						githubURL:
+							"https://github.com/thorski1/ECommerceClothingReact",
+						siteURL:
+							"https://sams-crwn-clothing.herokuapp.com/",
+						image: "/assets/CRWN.png"
+					}
+				];
